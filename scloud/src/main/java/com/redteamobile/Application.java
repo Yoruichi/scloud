07ba21c86497343892a5604412152a41a941137c
yoruichi <hz-zhangcong@163.com>
2017-12-22 11:24:38 +0800

    add configuration in main

--- scloud/src/main/java/com/redteamobile/Application.java
+++ scloud/src/main/java/com/redteamobile/Application.java
@@ -3,13 +3,15 @@ package com.redteamobile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args).getEnvironment();
     }
 
 }
