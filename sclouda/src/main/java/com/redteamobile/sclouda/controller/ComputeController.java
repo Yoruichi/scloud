68eb2fdb2ba74109242e4d8638273fca9f4396e6
yoruichi <hz-zhangcong@163.com>
2017-12-22 15:25:43 +0800

    add service instance log

--- sclouda/src/main/java/com/redteamobile/sclouda/controller/ComputeController.java
+++ sclouda/src/main/java/com/redteamobile/sclouda/controller/ComputeController.java
@@ -5,6 +5,7 @@ import com.redteamobile.sclouda.model.req.UserParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.client.serviceregistry.Registration;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,17 +18,18 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/compute")
 public class ComputeController extends BaseController {
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private LoadBalancerClient loadBalancerClient;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
-        ServiceInstance serviceInstance = discoveryClient.getLocalServiceInstance();
-        logger.info("/add, host:{},server:{},do {}+{} return {}", serviceInstance.getHost(),
+        ServiceInstance serviceInstance = loadBalancerClient.choose("compute-service");
+        logger.info("/add, host:{},port:{},server:{},do {}+{} return {}", serviceInstance.getHost(),
+                serviceInstance.getPort(),
                 serviceInstance.getServiceId(), a, b, a + b);
         return a + b;
     }
 
-    @RequestMapping(value="/test/json", method = RequestMethod.POST)
+    @RequestMapping(value = "/test/json", method = RequestMethod.POST)
     public ResponseStruct testJson(HttpServletRequest request, @RequestBody UserParam user) {
         logger.info("request received from host {} and server {} with parameter user {}", user);
         String headerParam = request.getHeader("tokenId");
