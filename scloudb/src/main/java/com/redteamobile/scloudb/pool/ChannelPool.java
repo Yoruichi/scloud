f00b9ca121b153279d43dfe21945dfc5af8565c0
yoruichi <hz-zhangcong@163.com>
2018-01-03 16:45:34 +0800

    enable netty server

--- scloudb/src/main/java/com/redteamobile/scloudb/pool/ChannelPool.java
+++ scloudb/src/main/java/com/redteamobile/scloudb/pool/ChannelPool.java
@@ -4,13 +4,18 @@ import io.grpc.ManagedChannel;
 import org.apache.commons.pool2.PooledObjectFactory;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by yoruichi on 17/9/13.
  */
 public class ChannelPool extends GenericObjectPool<ManagedChannel> {
+    private Logger logger = LoggerFactory.getLogger(ChannelPool.class);
+
     public ChannelPool(PooledObjectFactory<ManagedChannel> factory,
             GenericObjectPoolConfig config) {
         super(factory, config);
+        logger.info("Create pool for class [ManagedChannel].");
     }
 }
